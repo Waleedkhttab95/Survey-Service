e4401535801071ec505b185fc54744c07038897c
allordalex <allordalex10@gmail.com>
2018-07-30 20:02:14 +0300

    finish the auth

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '802825887953-oh3pol7vuuisvnbk0pbc9n7kpoepaij8.apps.googleusercontent.com',
+     googleClientSecret:'Xzolhw32YWjxf3kvX8Lwp4Y5',
+    cookieKey:'qwerty',
+    mongoURI : 'mongodb://Waleed:5823470a@ds259111.mlab.com:59111/emaily-dev95'
+    };
--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+const keys = require('./config/keys');
 const express=require('express');
+require('./models/user'); 
+require('./services/passport');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
+ const authRoutes = require('./routes/authRoutes');
+ const mongoose = require('mongoose');
+
 const app = express();
 
-app.get('',(req , res)=>{
-res.send({ hi:'كل خرا'})
-});
+app.use(
+cookieSession({
+ maxAge:30*24*60*60*1000,
+ keys: [keys.cookieKey]   
+})
+
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+mongoose.connect(keys.mongoURI,{ useNewUrlParser: true });
+
+authRoutes(app);
+
 
 const PORT  = process.env.PORT || 5000;
 app.listen(PORT);
