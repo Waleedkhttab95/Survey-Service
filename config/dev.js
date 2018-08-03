33339f37de726f3d46a3829b36eb11166b9ca462
allordalex <allordalex10@gmail.com>
2018-08-03 05:59:22 +0300

    Added billing and client app

--- config/dev.js
+++ config/dev.js
@@ -2,5 +2,7 @@ module.exports = {
     googleClientID: '802825887953-oh3pol7vuuisvnbk0pbc9n7kpoepaij8.apps.googleusercontent.com',
      googleClientSecret:'Xzolhw32YWjxf3kvX8Lwp4Y5',
     cookieKey:'qwerty',
-    mongoURI : 'mongodb://Waleed:5823470a@ds259111.mlab.com:59111/emaily-dev95'
+    mongoURI : 'mongodb://Waleed:5823470a@ds259111.mlab.com:59111/emaily-dev95',
+    stripePublishableKey: 'pk_test_i3syjK4jqGWYRUGIFb6tS9RR',
+    stripeSecretKey: 'sk_test_c6rbbMxvkjwblKf4yJaqrjfc'
     };
